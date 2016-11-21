387bfc9c00bf90a27db07f8dd0bad1f05f92a812
Splend811 <ryu__6@hotmail.com>
2016-11-21 20:33:37 +0100

    Added convert from int to real and from real to int

--- mapping.php
+++ mapping.php
@@ -14,40 +14,122 @@ else{
 	$addresses = htmlspecialchars($_POST['addresses']);
 }
 
+function FindTheNumber($data){
+	$data = explode(" ", $data);
+	$data = preg_grep("/1(\d+)/", $data);
+	return $data[array_rand($data,1)];
+
+}
+
 function DbMapping($data){
 
 $singelArray = explode("\t", $data); //Splite source address, destination address and signal type
 preg_match('/([a-zA-Z][a-zA-Z][0-9]{1,})\.(.+)/', $singelArray[0], $sourceAddressGroup);
 preg_match('/([a-zA-Z][a-zA-Z][0-9]{1,})\.(.+)/', $singelArray[1], $destinationAddressGroup);
 
-switch (strtolower($singelArray[2])) {
-	case "int":
-		$sourceAddressGroup[2] = preg_replace("[^0{1,3}]", "DBW", $sourceAddressGroup[2]);
-		$destinationAddressGroup[2] = preg_replace("[^0{1,3}]", "DBW", $destinationAddressGroup[2]);
-		echo "L ".$sourceAddressGroup[1].".".$sourceAddressGroup[2]."<br>";
-		echo "T ".$destinationAddressGroup[1].".".$destinationAddressGroup[2]."<br>";
-		break;
-	case "real":
-		$sourceAddressGroup[2] = preg_replace("[^0{1,3}]", "DBD", $sourceAddressGroup[2]);
-		$destinationAddressGroup[2] = preg_replace("[^0{1,3}]", "DBD", $destinationAddressGroup[2]);
-		echo "L ".$sourceAddressGroup[1].".".$sourceAddressGroup[2]."<br>";
-		echo "T ".$destinationAddressGroup[1].".".$destinationAddressGroup[2]."<br>";
-		break;
-	case "byte":
-		$sourceAddressGroup[2] = preg_replace("[^0{1,3}]", "DBB", $sourceAddressGroup[2]);
-		$destinationAddressGroup[2] = preg_replace("[^0{1,3}]", "DBB", $destinationAddressGroup[2]);
-		echo "L ".$sourceAddressGroup[1].".".$sourceAddressGroup[2]."<br>";
-		echo "T ".$destinationAddressGroup[1].".".$destinationAddressGroup[2]."<br>";
-		break;
-	default:
-		$sourceAddressGroup[2] = preg_replace("[^0{1,3}]", "DBX", $sourceAddressGroup[2]);
-		$destinationAddressGroup[2] = preg_replace("[^0{1,3}]", "DBX", $destinationAddressGroup[2]);
-		echo "A ".$sourceAddressGroup[1].".".$sourceAddressGroup[2]."<br>";
-		echo "= ".$destinationAddressGroup[1].".".$destinationAddressGroup[2]."<br>";
-		break;
-	}
+if (array_key_exists(3, $singelArray)) {
+	
+	switch (strtolower("$singelArray[2]+$singelArray[3]")) {
+		case "int+int":
+			$sourceAddressGroup[2] = preg_replace("[^0{1,3}]", "DBW", $sourceAddressGroup[2]);
+			$destinationAddressGroup[2] = preg_replace("[^0{1,3}]", "DBW", $destinationAddressGroup[2]);
+			echo "L ".$sourceAddressGroup[1].".".$sourceAddressGroup[2]."<br>";
+			echo "T ".$destinationAddressGroup[1].".".$destinationAddressGroup[2]."<br>";
+			echo "<br>";
+			break;
+		case "real+real":
+			$sourceAddressGroup[2] = preg_replace("[^0{1,3}]", "DBD", $sourceAddressGroup[2]);
+			$destinationAddressGroup[2] = preg_replace("[^0{1,3}]", "DBD", $destinationAddressGroup[2]);
+			echo "L ".$sourceAddressGroup[1].".".$sourceAddressGroup[2]."<br>";
+			echo "T ".$destinationAddressGroup[1].".".$destinationAddressGroup[2]."<br>";
+			echo "<br>";
+			break;
+		case "byte+byte":
+			$sourceAddressGroup[2] = preg_replace("[^0{1,3}]", "DBB", $sourceAddressGroup[2]);
+			$destinationAddressGroup[2] = preg_replace("[^0{1,3}]", "DBB", $destinationAddressGroup[2]);
+			echo "L ".$sourceAddressGroup[1].".".$sourceAddressGroup[2]."<br>";
+			echo "T ".$destinationAddressGroup[1].".".$destinationAddressGroup[2]."<br>";
+			echo "<br>";
+			break;
+		case "int+real":
+			$sourceAddressGroup[2] = preg_replace("[^0{1,3}]", "DBW", $sourceAddressGroup[2]);
+			$destinationAddressGroup[2] = preg_replace("[^0{1,3}]", "DBD", $destinationAddressGroup[2]);
+			echo "L ".$sourceAddressGroup[1].".".$sourceAddressGroup[2]."<br>";
+			echo "IND <br>";
+			echo "DTR <br>";
+			echo "/".FindTheNumber($singelArray[4])."<br>";
+			echo "T ".$destinationAddressGroup[1].".".$destinationAddressGroup[2]."<br>";
+			echo "<br>";
+			break;
+		case "real+int":
+			$sourceAddressGroup[2] = preg_replace("[^0{1,3}]", "DBD", $sourceAddressGroup[2]);
+			$destinationAddressGroup[2] = preg_replace("[^0{1,3}]", "DBW", $destinationAddressGroup[2]);
+			echo "L ".$sourceAddressGroup[1].".".$sourceAddressGroup[2]."<br>";
+			echo "RND <br>";
+			echo "DTI <br>";
+			echo "*".FindTheNumber($singelArray[4])."<br>";
+			echo "T ".$destinationAddressGroup[1].".".$destinationAddressGroup[2]."<br>";
+			echo "<br>";
+			break;
+		case "bit+bit":
+			$sourceAddressGroup[2] = preg_replace("[^0{1,3}]", "DBX", $sourceAddressGroup[2]);
+			$destinationAddressGroup[2] = preg_replace("[^0{1,3}]", "DBX", $destinationAddressGroup[2]);
+			echo "A ".$sourceAddressGroup[1].".".$sourceAddressGroup[2]."<br>";
+			echo "= ".$destinationAddressGroup[1].".".$destinationAddressGroup[2]."<br>";
+			echo "<br>";
+			break;
+		default:
+			$sourceAddressGroup[2] = preg_replace("[^0{1,3}]", "DBX", $sourceAddressGroup[2]);
+			$destinationAddressGroup[2] = preg_replace("[^0{1,3}]", "DBX", $destinationAddressGroup[2]);
+			echo "A ".$sourceAddressGroup[1].".".$sourceAddressGroup[2]."<br>";
+			echo "= ".$destinationAddressGroup[1].".".$destinationAddressGroup[2]."<br>";
+			echo "<br>";
+			break;
+		}
+}
+else{
+	switch (strtolower($singelArray[2])) {
+		case "int":
+			$sourceAddressGroup[2] = preg_replace("[^0{1,3}]", "DBW", $sourceAddressGroup[2]);
+			$destinationAddressGroup[2] = preg_replace("[^0{1,3}]", "DBW", $destinationAddressGroup[2]);
+			echo "L ".$sourceAddressGroup[1].".".$sourceAddressGroup[2]."<br>";
+			echo "T ".$destinationAddressGroup[1].".".$destinationAddressGroup[2]."<br>";
+			echo "<br>";
+			break;
+		case "real":
+			$sourceAddressGroup[2] = preg_replace("[^0{1,3}]", "DBD", $sourceAddressGroup[2]);
+			$destinationAddressGroup[2] = preg_replace("[^0{1,3}]", "DBD", $destinationAddressGroup[2]);
+			echo "L ".$sourceAddressGroup[1].".".$sourceAddressGroup[2]."<br>";
+			echo "T ".$destinationAddressGroup[1].".".$destinationAddressGroup[2]."<br>";
+			echo "<br>";
+			break;
+		case "byte":
+			$sourceAddressGroup[2] = preg_replace("[^0{1,3}]", "DBB", $sourceAddressGroup[2]);
+			$destinationAddressGroup[2] = preg_replace("[^0{1,3}]", "DBB", $destinationAddressGroup[2]);
+			echo "L ".$sourceAddressGroup[1].".".$sourceAddressGroup[2]."<br>";
+			echo "T ".$destinationAddressGroup[1].".".$destinationAddressGroup[2]."<br>";
+			echo "<br>";
+			break;
+		case "bit":
+			$sourceAddressGroup[2] = preg_replace("[^0{1,3}]", "DBX", $sourceAddressGroup[2]);
+			$destinationAddressGroup[2] = preg_replace("[^0{1,3}]", "DBX", $destinationAddressGroup[2]);
+			echo "A ".$sourceAddressGroup[1].".".$sourceAddressGroup[2]."<br>";
+			echo "= ".$destinationAddressGroup[1].".".$destinationAddressGroup[2]."<br>";
+			echo "<br>";
+			break;
+		default:
+			$sourceAddressGroup[2] = preg_replace("[^0{1,3}]", "DBX", $sourceAddressGroup[2]);
+			$destinationAddressGroup[2] = preg_replace("[^0{1,3}]", "DBX", $destinationAddressGroup[2]);
+			echo "A ".$sourceAddressGroup[1].".".$sourceAddressGroup[2]."<br>";
+			echo "= ".$destinationAddressGroup[1].".".$destinationAddressGroup[2]."<br>";
+			echo "<br>";
+			break;
+		}
+
+}
 	return;
 }
+
 
 ?>
 
